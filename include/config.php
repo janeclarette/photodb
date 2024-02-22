0e4d8797f68fab39188531f9194ae279321044bf
Raymond Lei Nogalo <raymondlei.nogalo@tup.edu.ph>
2024-02-22 10:10:06 +0800

    wala
    awalawlalsmdandklw

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_passwd = "";
-$db_name = "photodb2";
+$db_name = "photodb";
 $conn = mysqli_connect($db_host, $db_username, $db_passwd,$db_name);
 
 if(!$conn)
