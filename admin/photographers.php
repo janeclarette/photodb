d58124590f314cc2dfbf0eaeb2c1965d8c077b54
janeclarette <clarette827@gmail.com>
2024-02-24 14:42:46 +0800

    header

---
+++ admin/photographers.php
@@ -0,0 +1,5 @@
+<?php
+// Include necessary files and establish a database connection
+include("../include/config.php");
+include("../admin/adminheader.php"); // Include your database connection
+?>
