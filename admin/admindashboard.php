7ab679e3dd6065b89fc16e82109d08dcf7a0ac5a
janeclarette <clarette827@gmail.com>
2024-02-29 10:24:16 +0800

    ad

--- admin/admindashboard.php
+++ admin/admindashboard.php
@@ -2,5 +2,5 @@
 // Include necessary files and establish a database connection
 include("../include/config.php");
 include("../admin/adminheader.php"); // Include your database connection
-include("../admin/sales.php");
+
 ?>
--- admin/sales.php
+++ admin/sales.php
@@ -1,5 +1,6 @@
 <?php
 include("../include/config.php");
+include("../admin/adminheader.php");
 
 
 // Monthly data and most booked photographer variables
